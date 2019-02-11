8ab54d5d48e4de1102f2c94e9184591dbb435f41
Maxime Daniel <maxaxeldaniel@gmail.com>
2019-02-11 14:55:44 +0100

    'test'

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('babel-register')({
 
 var server = require('./server');
 
-var port = process.env.API_PORT || 3001;
+var port =  3001;
 server.listen(port, function() {
     console.log(`api running on ${port}`);
 });
