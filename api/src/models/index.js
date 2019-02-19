d6db93a065aa7b764efa3f601ea4428912759066
Maxime Daniel <maxaxeldaniel@gmail.com>
2019-02-19 10:26:50 +0100

    debugging mongodb docker

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/rezbuild", { useMongoClient: true });
+mongoose.connect("mongodb://0.0.0.0:27017/rezbuild", { useMongoClient: true });
 module.exports.User = require('./user');
 module.exports.Project = require('./project');
 module.exports.Revision = require('./revision');
