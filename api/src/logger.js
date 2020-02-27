41e7561a972fbbc807dc1d2e7941bde3df18c48c
maximedaniel <maxaxeldaniel@gmail.com>
2020-02-27 14:17:00 +0100

    Debugging logger issues and adjusting file explorer view

--- api/src/logger.js
+++ api/src/logger.js
@@ -8,7 +8,7 @@ if ( !fs.existsSync( logDir ) ) {
     // Create the directory if it does not exist
     fs.mkdirSync( logDir );
 }
-
+var sanitize = (unsanitized) =>  unsanitized.replace(/\\/g, "\\\\").replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t").replace(/\f/g, "\\f").replace(/"/g,"\\\"").replace(/'/g,"\\\'").replace(/\&/g, "\\&");
 const customFormat = format.printf(log => `${log.timestamp} | ${log.level}: ${log.message}`);
 const levels = { 
     error: 0,
@@ -45,11 +45,11 @@ var logger = createLogger({
     exitOnError: false
   })
 
-console.log = (message) => logger.log({level: 'verbose', message:message, color:'purple'});
-console.debug = (message) => logger.log({level: 'debug', message:message, color:'blue'});
-console.info = (message) => logger.log({level: 'info', message:message, color:'green'});
-console.warn = (message) => logger.log({level: 'warn', message:message, color:'orange'});
-console.error = (message) => logger.log({level: 'error', message:message, color:'red'});
+console.log = (message) => logger.log({level: 'verbose', message:sanitize(message), color:'purple'});
+console.debug = (message) => logger.log({level: 'debug', message:sanitize(message), color:'blue'});
+console.info = (message) => logger.log({level: 'info', message:sanitize(message), color:'green'});
+console.warn = (message) => logger.log({level: 'warn', message:sanitize(message), color:'orange'});
+console.error = (message) => logger.log({level: 'error', message:sanitize(message), color:'red'});
 
 process.on('uncaughtException', function (error) {
     console.error(error);
